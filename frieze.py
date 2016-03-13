d18e81bb83c02d3d4715e854f74b1587e6ffb17e
Marielle Foster <marielleefoster@gmail.com>
2016-03-13 17:50:00 -0500

    fixing things up
    logic, organization, presentation, etc.

--- frieze.py
+++ frieze.py
@@ -7,7 +7,6 @@ def find_furthest_x(side, vertices):
         for i in vertices:
             if i.x < x:
                 x = i.x
-
         return x
 
     if side == "r":
@@ -24,7 +23,6 @@ def is_possible(vertices):
     return True
 
 def draw_frieze(win, vertices, frieze, trans):
-
     #shift over to left
     x_left = find_furthest_x("l", vertices)
 
@@ -40,7 +38,7 @@ def draw_frieze(win, vertices, frieze, trans):
         pattern = Polygon(vertices)
         pattern.setFill('gray')
         pattern.setOutline('black')
-        pattern.setWidth(4)  # width of boundary line
+        pattern.setWidth(2)  # width of boundary line
         pattern.draw(win)
 
         if frieze == 1:
@@ -49,7 +47,7 @@ def draw_frieze(win, vertices, frieze, trans):
 
         if frieze == 2:
             for v in vertices:
-                v.x = v.x + trans
+                v.x = v.x + trans/2
                 v.y = 600 - v.y
             
         if frieze > 2:
@@ -58,18 +56,23 @@ def draw_frieze(win, vertices, frieze, trans):
         if frieze == 3:
             for v in vertices:
                 v.x = 2*r - v.x + 5
+                if alt == False:
+                    v.x = v.x + trans
             
         
         if frieze == 4:
             for v in vertices:
                 v.x = 2*r - v.x + 5
                 v.y = 600 - v.y
+                if alt == True:
+                    v.x = v.x + trans
         
         if frieze == 5:
             for v in vertices:
                 v.x = 2*r - v.x + 5 
 
-                if alt == True:
+                if alt == False:
+                    v.x = v.x + trans
                     v.y = 600 - v.y
         
         if frieze == 6:
@@ -87,11 +90,7 @@ def draw_frieze(win, vertices, frieze, trans):
                         v.x = v.x + trans
                 else:
                     v.y = 600 - v.y
-                    
-                    
             mod4 += 1
-
-
 
         if alt == True:
             alt = False
@@ -101,23 +100,17 @@ def draw_frieze(win, vertices, frieze, trans):
         possible = is_possible(vertices)
 
 
-#   7:
-#       1st: y = -y
-#       2nd: x = 2f - x
-
-
-
 from graphics import *
 
 def main():
     num_points = input("How many points do you want for your polygon? ")
     num_points = int(num_points)
+    # num_points = 3
 
     win = GraphWin('Frieze Patterns', 1000, 600)
 
     win.setBackground('white')
     
-
     square = Rectangle(Point(425, 275), Point(575, 125))
     square.draw(win)
 
@@ -127,19 +120,23 @@ def main():
     message.setSize(20)
     message.draw(win)
 
-    # Get and draw vertices of polygon
+    # # Get and draw vertices of polygon
     vertices = []
     for i in range(num_points):
         pt = win.getMouse()
         pt.draw(win)
         vertices.append(pt)
 
+    # vertices = [Point(505, 146), Point(479, 198), Point(525, 236)]
+    # for v in vertices:
+    #     print v.x, v.y
 
-    # Use Polygon object to draw the triangle
+
+    # Use Polygon object to draw the object
     pattern = Polygon(vertices)
     pattern.setFill('gray')
     pattern.setOutline('black')
-    pattern.setWidth(4)  # width of boundary line
+    pattern.setWidth(2)  # width of boundary line
     pattern.draw(win)
 
     message.setText('Please type in the terminal which frieze group and translation you want')
@@ -147,13 +144,12 @@ def main():
     frieze = input("Which frieze group do you want (any number 1-7)? ")
     frieze = int(frieze)
 
-
     while frieze < 1 and frieze > 7:
         frieze = input("That was wrong. Which frieze group do you want (any number 1-7)? ")
         frieze = int(frieze)
 
     trans = input("How big do you want the translation factor to be? (under 100 recommended) ")
-
+    # trans = 80
     pattern.undraw()
     square.undraw()
     for pt in vertices:
